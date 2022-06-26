fdc6cd0a139aa59dd7414518092e827148343f6e
plattegruber <gruberplatte@gmail.com>
2022-06-26 17:54:26 -0500

    transpile headlessui dependency

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,12 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+    target: 'static',
     css: ["@/assets/css/styles.css"],
     build: {
       postcss: {
         postcssOptions: require("./postcss.config.js"),
       },
+      transpile: ['@headlessui/vue']
     },
 })
