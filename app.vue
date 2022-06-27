0b7edffc345fd9425ed961d19655bf758e90b3ad
plattegruber <gruberplatte@gmail.com>
2022-06-26 22:44:43 -0500

    update global background color

--- app.vue
+++ app.vue
@@ -89,7 +89,7 @@
         </div>
       </div>
     </div>
-    <div class="md:pl-64 flex flex-col flex-1">
+    <div class="md:pl-64 flex flex-col flex-1 bg-slate-100">
       <div class="sticky top-0 z-10 md:hidden pl-1 pt-1 sm:pl-3 sm:pt-3 bg-gray-100">
         <button type="button" class="-ml-0.5 -mt-0.5 h-12 w-12 inline-flex items-center justify-center rounded-md text-gray-500 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500" @click="sidebarOpen = true">
           <span class="sr-only">Open sidebar</span>
