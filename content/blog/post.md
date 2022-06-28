6339bf9c1b31f054cb8b8f9d693ca82f48ff68dc
plattegruber <gruberplatte@gmail.com>
2022-06-27 19:49:20 -0500

    add blog list to blog index

--- content/blog/post.md
+++ content/blog/post.md
@@ -1,2 +1,7 @@
+---
+title: This is my first blog post
+description: And this is my description
+---
+
 # yo whatup
 okrrr
--- pages/blog.vue
+++ pages/blog.vue
@@ -1,5 +1,11 @@
 <template>
     <div>
-        blog yerrrr
+        <ContentList path="/blog" v-slot="{ list }">
+            <div v-for="article in list" :key="article._path">
+                <h2>{{ article.title }}</h2>
+                <p>{{ article.description }}</p>
+                <NuxtLink :to="article._path">go</NuxtLink>
+            </div>
+        </ContentList>
     </div>
 </template>
