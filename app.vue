90d3c70f25fb0d2290b8bfe7e82acafe3420a5d1
plattegruber <gruberplatte@gmail.com>
2022-06-28 17:32:44 -0500

    fix profile image size and shape

--- app.vue
+++ app.vue
@@ -52,7 +52,7 @@
       <div class="flex-1 flex flex-col min-h-0 border-r border-gray-200 bg-white">
         <div class="flex-1 flex flex-col pt-5 pb-4 overflow-y-auto">
           <div class="flex items-center flex-shrink-0 px-4">
-            <img class="h-8 w-auto" src="~/assets/images/headshot.jpg" alt="Platte Gruber" />
+            <img class="rounded-full" src="~/assets/images/cropped_headshot.jpg" alt="Platte Gruber" />
           </div>
           <nav class="mt-5 flex-1 px-2 bg-white space-y-1">
             <a v-for="item in navigation" :key="item.name" :href="item.href" :class="[item.current ? 'bg-gray-100 text-gray-900' : 'text-gray-600 hover:bg-gray-50 hover:text-gray-900', 'group flex items-center px-2 py-2 text-sm font-medium rounded-md']">
